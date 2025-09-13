9e50cba72cd10d890e4d543785dad2f6dabbcc5b
Luis Felipe Caraballo <luis.caraballo7@upr.edu>
2025-09-13 20:58:58 +0000

    Lab3

--- main.cpp
+++ main.cpp
@@ -13,24 +13,36 @@
 
  int main() {
 
+   //Inicializar el generador de numeros aleatorios con la hora actual.
+
     srand(time(0));
+
+   //Declarar variables
 
     int numero1, numero2, numero3;
     int auxiliar; 
+
+   //Generar tres enteros en el rango [0,100].
 
     numero1 = rand() % 101;
     numero2 = rand() % 101;
     numero3 = rand() % 101;
 
+   //Mostrar numeros generados sin ordenar.
+
     std::cout << "Numeros aleatorios generados: "
               << numero1 << "," << numero2 << "," << numero3 << std::endl;
+    
+   // Ordenar de mayor a menor con comparaciones.
 
     if (numero1 < numero2) { auxiliar = numero1; numero1 = numero2; numero2 = auxiliar; }
     if (numero1 < numero3) { auxiliar = numero1; numero1 = numero3; numero3 = auxiliar; }
     if (numero2 < numero3) { auxiliar = numero2; numero2 = numero3; numero3 = auxiliar; }
 
+   //Mostrar en  orden descendente
+
     std::cout << "Orden descendiente: "
-              << numero1 << " >=" << numero2 << ">=" << numero3 << std::endl;
+              << numero1 << ">=" << numero2 << ">=" << numero3 << std::endl;
     return 0;
  }
 
