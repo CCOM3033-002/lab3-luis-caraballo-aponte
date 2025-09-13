54084731e02cf1e288d9864bf3e95b77cf574d76
Luis Felipe Caraballo <luis.caraballo7@upr.edu>
2025-09-13 20:41:58 +0000

    lab_3

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+//Luis Caraballo Aponte
+//CCOM3033-002
+
+/*Descripción:
+  Programa en C++ que genera tres números enteros aleatorios entre 0 y 100 (incluidos).
+  Primero los despliega en el orden generado y luego los muestra en orden descendente.
+  Se utiliza srand(time(0)) para garantizar que en cada ejecución los números sean distintos.
+ */
+
+ #include <iostream>
+ #include <cstdlib>
+ #include < ctime> 
 
